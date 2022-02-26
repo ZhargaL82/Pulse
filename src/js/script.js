686566535ef74a3d17b24695dc8bf5744171234f
ZhargaL82 <zhl82@ulanovka.ru>
2022-02-26 10:16:38 +0800

    git add -A

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
-        dots: true;
-        slidesToShow: 2;
-        slidesToScroll; 2
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                dots: true,
+                arrows: false
+            }
+        }]
     });
-  });
+});
