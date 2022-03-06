ec0eef82102e774ef6f3dca5038b9b2a8b69e4eb
ZhargaL82 <zhl82@ulanovka.ru>
2022-03-06 11:57:24 +0800

    git add -A

--- src/js/script.js
+++ src/js/script.js
@@ -48,7 +48,36 @@ $(document).ready(function(){
         });
     });
 
-    $('.feed-form').validate();
-    $('#consultation form').validate();
-    $('#order form').validate();
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символов!")
+                },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    }
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name="phone"]').mask("+7 (999) 999-99-99");
 });
