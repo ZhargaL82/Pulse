68db57e424a1df643a65c98a77873e35ce81bb90
ZhargaL82 <zhl82@ulanovka.ru>
2022-03-07 13:17:17 +0800

    git add -A

--- src/js/script.js
+++ src/js/script.js
@@ -80,4 +80,50 @@ $(document).ready(function(){
     validateForms('#order form');
 
     $('input[name="phone"]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+
+        if (!$(this).valid()) {
+            return;
+        }
+
+        $.ajax({
+            type:  "POST",
+            url: "mailer/send.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    function send(event, php){
+        console.log("Отправка запроса");
+        event.preventDefault ? event.preventDefault() : event.returnValue = false;
+        var req = new XMLHttpRequest();
+        req.open('POST', php, true);
+        req.onload = function() {
+            if (req.status >= 200 && req.status < 400) {
+            json = JSON.parse(this.response); // Ебанный internet explorer 11
+                console.log(json);
+                
+                // ЗДЕСЬ УКАЗЫВАЕМ ДЕЙСТВИЯ В СЛУЧАЕ УСПЕХА ИЛИ НЕУДАЧИ
+                if (json.result == "success") {
+                    // Если сообщение отправлено
+                    alert("Сообщение отправлено");
+                } else {
+                    // Если произошла ошибка
+                    alert("Ошибка. Сообщение не отправлено");
+                }
+            // Если не удалось связаться с php файлом
+            } else {alert("Ошибка сервера. Номер: "+req.status);}}; 
+        
+        // Если не удалось отправить запрос. Стоит блок на хостинге
+        req.onerror = function() {alert("Ошибка отправки запроса");};
+        req.send(new FormData(event.target));
+    }
 });
